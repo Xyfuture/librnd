4d7afd15b09f10201b790259562e6b1aac5a2baa
Xyfuture <49277529+Xyfuture@users.noreply.github.com>
2022-09-03 13:42:12 +0800

    Update xmake.lua
    fix ** and *

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ add_requires("libprim")
 
 target("librnd")
     set_kind("static")
-    add_headerfiles("src/**/*.h")
+    add_headerfiles("src/**.h")
     add_files("src/**.cc|**_TEST.cc")
     add_includedirs("src/")
     add_packages("libprim")
